a0b3657ed37fa5e83adde1f4e282b4cdd748401c
Sandor Dargo <sandordargo@github.com>
2020-12-15 21:48:15 +0100

    Add article analytics

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, request
+from analytics import article_analytics, article_fetcher
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     api_key = request.args.get('apiKey', default="", type=str)
-    response = jsonify(message=f'Hello {api_key}, this is DEV analytics here!')
+    articles = article_fetcher.get_articles_from_dev(api_key)
+    response = jsonify(article_analytics.analyse(articles))
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
-
 
 if __name__ == "__main__":
     app.run()
